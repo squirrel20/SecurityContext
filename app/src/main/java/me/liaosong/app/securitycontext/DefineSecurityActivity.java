7bcb13246451131c5db294c81080089eff8231d5
squirrel <ls200853@gmail.com>
2015-02-02 22:06:10 +0800

    添加了listView

--- app/src/main/java/me/liaosong/app/securitycontext/DefineSecurityActivity.java
+++ app/src/main/java/me/liaosong/app/securitycontext/DefineSecurityActivity.java
@@ -4,14 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class DefineSecurityActivity extends ActionBarActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_define_security);
+
+        listView = (ListView) this.findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 
 
